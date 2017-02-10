59e835d3884f08e94c43e0afea09aff684e580c6
saim khan <saim.khan@colorado.edu>
2017-02-10 04:05:54 -0700

    added function to plot graphs

--- logreg/graph_generate.py
+++ logreg/graph_generate.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 x1=[]
 y1=[]
 x2=[]
@@ -16,15 +17,26 @@ with open("graph_plot.txt") as fh:
             flag=0.3
         elif flag==0.1:
             x,y=line.split(":")
-            x1.append(x)
-            y1.append(y)
+            x1.append(float(x))
+            y1.append(float(y))
         elif flag==0.2:
             x,y=line.split(":")
-            x2.append(x)
-            y2.append(y)
+            x2.append(float(x))
+            y2.append(float(y))
         elif flag==0.3:
             x,y=line.split(":")
-            x3.append(x)
-            y3.append(y)
+            x3.append(float(x))
+            y3.append(float(y))
         else:
             continue
+
+plt.plot(x1,y1,linewidth=1.5)
+plt.plot(x2,y2,linewidth=1.5)
+plt.plot(x3,y3,linewidth=1.5)
+
+
+plt.title('Learning rate affect on convergence')
+plt.ylabel('ho_acc')
+plt.xlabel('iteration')
+
+plt.show()
